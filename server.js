3509fb68defe4bce12ccb801abd283899917adb1
raul-f <raulf.dev@gmail.com>
2019-03-14 12:53:54 -0300

    test logic complete

--- server.js
+++ server.js
@@ -1,57 +1,33 @@
-'use strict';
+'use strict'
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const app = express()
 
-var app = express();
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.use(cors({origin: '*'})); //For FCC testing purposes only
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route('/').get(function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
-//For FCC testing purposes
-fccTestingRoutes(app);
+// Routing
 
-//Routing for API 
-apiRoutes(app);  
-    
 //404 Not Found Middleware
 app.use(function(req, res, next) {
-  res.status(404)
+  res
+    .status(404)
     .type('text')
-    .send('Not Found');
-});
+    .send('Not Found')
+})
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 3500);
-  }
-});
+const listener = app.listen(process.env.PORT || 3000, function() {
+  console.log(`Listening on port ${listener.address().port}`)
+})
 
-module.exports = app; //for testing
+module.exports = app //for testing
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,50 +1,125 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
-var assert = chai.assert;
-var server = require('../server');
+var chaiHttp = require('chai-http')
+var chai = require('chai')
+var assert = chai.assert
+var server = require('../server')
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
+
+let likes = { count: 0 }
 
 suite('Functional Tests', function() {
-    
-    suite('GET /api/stock-prices => stockData object', function() {
-      
-      test('1 stock', function(done) {
-       chai.request(server)
+  suite('GET /api/stock-prices => stockData object', function() {
+    test('1 stock', function(done) {
+      chai
+        .request(server)
         .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-          
-          //complete this one too
-          
-          done();
-        });
-      });
-      
-      test('1 stock with like', function(done) {
-        
-      });
-      
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
-      });
-      
-      test('2 stocks', function(done) {
-        
-      });
-      
-      test('2 stocks with like', function(done) {
-        
-      });
-      
-    });
+        .query({ stock: 'goog' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200)
+          assert.isObject(res.body)
+          assert.isObject(res.body.stockData)
+          const data = res.body.stockData
+          assert.property(data, 'stock')
+          assert.typeOf(data.stock, 'string')
+          assert.property(data, 'price')
+          assert.equal(data.price, parseFloat(stock.price))
+          assert.property(data, 'likes')
+          assert.typeOf(data.likes, 'number')
+          likes.count = data.likes
+          done()
+        })
+    })
 
-});
+    test('1 stock with like', function(done) {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'goog', like: 1 })
+        .end(function(err, res) {
+          assert.equal(res.status, 200)
+          assert.isObject(res.body)
+          assert.isObject(res.body.stockData)
+          const data = res.body.stockData
+          assert.property(data, 'stock')
+          assert.typeOf(data.stock, 'string')
+          assert.property(data, 'price')
+          assert.equal(data.price, parseFloat(stock.price))
+          assert.property(data, 'likes')
+          assert.typeOf(data.likes, 'number')
+          assert.equal(data.likes, likes.count + 1)
+          done()
+        })
+    })
+
+    test('1 stock with like again (ensure likes arent double counted)', function(done) {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'goog', like: 1 })
+        .end(function(err, res) {
+          assert.equal(res.status, 200)
+          assert.isObject(res.body)
+          assert.isObject(res.body.stockData)
+          const data = res.body.stockData
+          assert.property(data, 'stock')
+          assert.typeOf(data.stock, 'string')
+          assert.property(data, 'price')
+          assert.equal(data.price, parseFloat(stock.price))
+          assert.property(data, 'likes')
+          assert.typeOf(data.likes, 'number')
+          assert.equal(data.likes, likes.count + 1)
+          done()
+        })
+    })
+
+    test('2 stocks', function(done) {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: ['goog', 'msft'] })
+        .end(function(err, res) {
+          assert.equal(res.status, 200)
+          assert.isObject(res.body)
+          assert.isArray(res.body.stockData)
+          for (const data of res.body.stockData) {
+            assert.property(data, 'stock')
+            assert.typeOf(data.stock, 'string')
+            assert.property(data, 'price')
+            assert.equal(data.price, parseFloat(stock.price))
+            assert.property(data, 'rel_likes')
+            assert.typeOf(data.rel_likes, 'number')
+          }
+          done()
+        })
+    })
+
+    test('2 stocks with like', function(done) {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: ['goog', 'msft'], like: true })
+        .end(function(err, res) {
+          assert.equal(res.status, 200)
+          assert.isObject(res.body)
+          assert.isArray(res.body.stockData)
+          for (const data of res.body.stockData) {
+            assert.property(data, 'stock')
+            assert.typeOf(data.stock, 'string')
+            assert.property(data, 'price')
+            assert.equal(data.price, parseFloat(stock.price))
+            assert.property(data, 'rel_likes')
+            assert.typeOf(data.rel_likes, 'number')
+          }
+          done()
+        })
+    })
+  })
+})
