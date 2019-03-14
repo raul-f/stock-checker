4224105902f519bede57d906a6dd84105d32eac0
raul-f <raulf.dev@gmail.com>
2019-03-14 14:18:59 -0300

    all tests passing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
 
 const app = express()
 
@@ -16,6 +17,70 @@ app.route('/').get(function(req, res) {
 })
 
 // Routing
+
+app.get('/api/stock-prices', async (req, res) => {
+  const stock = req.query.stock
+  if (Array.isArray(stock)) {
+    if (stock.length === 2) {
+      const stock1return = await fetch(
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock[0].toUpperCase()}&apikey=U31YIC4VSJAL65M3`
+      )
+      const stock2return = await fetch(
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock[1].toUpperCase()}&apikey=U31YIC4VSJAL65M3`
+      )
+      const stock1Data = await stock1return.json()
+      const stock2Data = await stock2return.json()
+
+      const returned = {
+        stockData: [
+          {
+            stock: stock1Data['Meta Data']['2. Symbol'],
+            price:
+              stock1Data['Time Series (Daily)'][new Date().toISOString().substring(0, 10)][
+                '4. close'
+              ],
+          },
+          {
+            stock: stock2Data['Meta Data']['2. Symbol'],
+            price:
+              stock2Data['Time Series (Daily)'][new Date().toISOString().substring(0, 10)][
+                '4. close'
+              ],
+          },
+        ],
+      }
+      res.json(returned)
+    } else if (stock.length === 1) {
+      const dataReturn = await fetch(
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock[0].toUpperCase()}&apikey=U31YIC4VSJAL65M3`
+      )
+      const data = await dataReturn.json()
+      const returned = {
+        stockData: {
+          stock: data['Meta Data']['2. Symbol'],
+          price: data['Time Series (Daily)'][new Date().toISOString().substring(0, 10)]['4. close'],
+        },
+      }
+      console.log(returned)
+      res.json(returned)
+    } else {
+      res.send('Given stock is invalid')
+    }
+  } else {
+    const dataReturn = await fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock.toUpperCase()}&apikey=U31YIC4VSJAL65M3`
+    )
+    const data = await dataReturn.json()
+
+    const returned = {
+      stockData: {
+        stock: data['Meta Data']['2. Symbol'],
+        price: data['Time Series (Daily)'][new Date().toISOString().substring(0, 10)]['4. close'],
+      },
+    }
+    res.json(returned)
+  }
+})
 
 //404 Not Found Middleware
 app.use(function(req, res, next) {
